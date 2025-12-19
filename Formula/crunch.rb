f707648f326e38dc07f7151b8d988d64f54129e7
byteoxo <byteoxo@qiya.io>
2025-12-19 13:22:54 +0800

    update crunch cli

--- Formula/crunch.rb
+++ Formula/crunch.rb
@@ -1,8 +1,8 @@
 class Crunch < Formula
   desc "A easiest, blazingly fast parallel media compression tool for normal people"
   homepage "https://github.com/byteoxo/crunch"
-  url "https://github.com/byteoxo/crunch/releases/download/v1.0.0/mycli-1.0.0.tar.gz"
-  sha256 "abc123..."  # Generate with: shasum -a 256 mycli-1.0.0.tar.gz
+  url "https://github.com/byteoxo/crunch/releases/download/v0.2.0/crunch.tar.gz"
+  sha256 "887427e5a41f1d732f763cab030d5285e1ce6beeddc96f20dd8791d00aa02ab4"
   license "MIT"
   version "1.0.0"
 
